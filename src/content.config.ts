945a87163b553b28e0a63182790c85edf2f467f5
John Dorion <john@johndorion.com>
2026-01-14 11:17:50 -0600

    bug fixes and such

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,33 +13,13 @@ const blogSchema = z.object({
     }).optional(),
 });
 
-const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-});
-
 export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({
     loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
     schema: blogSchema,
 });
 
-const storeCollection = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/store" }),
-    schema: storeSchema,
-});
-
 export const collections = {
     'blog': blogCollection,
-    'store': storeCollection
 }
